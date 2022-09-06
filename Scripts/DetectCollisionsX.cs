c82e6a443ade347740f431b1c38b6c588fae5b5f
princehalo <princehalo@ymail.com>
2022-09-06 01:14:58 +0100

    Commit Updated

---
+++ Scripts/DetectCollisionsX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+
+    public GameObject dogPrefab;
+
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
